4febab6086d8279f0976a3b228ac9b7fab20275f
rouzbehezzati <35449792+rouzbehezzati@users.noreply.github.com>
2018-04-21 00:53:35 +0430

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  push: {
+      ios: [
+      {
+          pfx: process.env.PFX_URL,
+          bundleId: process.env.BUNDLE_ID,
+          production: false
+      },
+      {
+          pfx: process.env.PFX_URL,
+          bundleId: process.env.BUNDLE_ID,
+          production: true
+      }
+    ]
+},
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
