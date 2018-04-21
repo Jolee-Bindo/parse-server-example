ef717e0f17ff6917222045b931dd00a7118d8a0e
rouzbehezzati <35449792+rouzbehezzati@users.noreply.github.com>
2018-04-21 06:13:16 +0430

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -8,11 +8,9 @@ Parse.Cloud.define('sendPushNotification', function(request, response) {
         var message = request.params.message;
         var queryUser = new Parse.Query(Parse.User);
         queryUser.equalTo('objectId', userId);
-          console.log(queryUser);
-
+  
         var query = new Parse.Query(Parse.Installation);
         query.matchesQuery('user', queryUser);
-          console.log(query);
 
         Parse.Push.send({
           where: query,
