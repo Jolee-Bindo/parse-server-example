64b1a72d58881227e358895781c363c0222851da
rouzbehezzati <35449792+rouzbehezzati@users.noreply.github.com>
2018-04-21 04:46:39 +0430

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,31 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define("sendPushNotification", function(request, response) {
+        var userId = request.params.userId;
+        var message = request.params.message;
+        var queryUser = new Parse.Query(Parse.User);
+        queryUser.equalTo('objectId', userId);
+        var query = new Parse.Query(Parse.Installation);
+        query.matchesQuery('user', queryUser);
+
+        Parse.Push.send({
+          where: query,
+          data: {
+            alert: message,
+            badge: 1,
+            sound: 'default'
+          }
+        }, {
+          success: function() {
+            console.log('##### PUSH OK');
+            response.success();
+          },
+          error: function(error) {
+            console.log('##### PUSH ERROR');
+            response.error('ERROR');
+          },
+          useMasterKey: true
+        });
+});
