84287f182b5c068553a2b8a7bd3c87a08066ac60
rouzbehezzati <35449792+rouzbehezzati@users.noreply.github.com>
2018-05-10 19:09:04 +0430

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -31,3 +31,74 @@ Parse.Cloud.define('sendPushNotification', function(request, response) {
           }
         });
 });
+
+Parse.Cloud.define('deactivateEvent', function(request, response) {
+  var bookingDayId = request.params.bookingDayId;
+  var bookingEvent = request.params.bookingEvent;
+  var query = new Parse.Query(Parse.BookingDay);
+  query.equalTo('objectId', bookingDayId);
+  query.include('bookingTickets');
+  query.first({
+  success: function(object) {
+    // Successfully retrieved the object.
+    var bookingDay = object;
+    var bookingTickets = bookingDay.get("bookingTickets");
+    for (var i = 0; i < bookingTickets.length; i++) {
+      var bookingTicket = bookingTickets[i];
+      var bookingTicketStatus = bookingTicket.get('bookingTicketStatus');
+      if (bookingTicketStatus == 'bookingTicketStatusBookedByBusiness' || bookingTicketStatus == 'bookingTicketStatusBookedByClient') {
+        var CancelledBooking = Parse.Object.extend("CancelledBooking");
+        var cancelledBooking = new CancelledBooking();
+
+        cancelledBooking.set("cancellationStatus", 'bookingTicketStatusCancelledByBusiness');
+        cancelledBooking.set("cancelledBookingTicket", bookingTicket);
+        var business = bookingTicket.get('business');
+        cancelledBooking.set("cancelledBookingBusiness", business);
+        [cancelBookingInfo setObject:[NSDate date] forKey:kCancellationDate];
+
+        var bookingTicketClientStatus = bookingTicket.get('bookingTicketClientStatus');
+        if (bookingTicketClientStatus == 'bookingTicketClientStatusRegistered') {
+          var client = bookingTicket.get('bookingTicketClient');
+          cancelledBooking.set("cancelledBookingClient", client);
+        } else if (bookingTicketClientStatus == 'bookingTicketClientStatusGuest') {
+          var client = bookingTicket.get('bookingTicketGuestClient');
+          cancelledBooking.set("cancelledBookingGuestClient", client);
+        }
+
+        cancelledBooking.save(null, {
+          success: function(cancelledBooking) {
+            alert('New object created with objectId: ' + cancelledBooking.id);
+            var numberOfReservedBookingsPerDay = bookingDay.get("numberOfReservedBookingsPerDay");
+            bookingDay.set("numberOfReservedBookingsPerDay", numberOfReservedBookingsPerDay - 1);
+            var numberOfAvailableBookingsPerDay = bookingDay.get("numberOfAvailableBookingsPerDay");
+            bookingDay.set("numberOfAvailableBookingsPerDay", numberOfAvailableBookingsPerDay + 1);
+            bookingDay.set("bookingEventStatus", 'bookingEventStatusNotActive');
+            bookingDay.save();
+            
+            var bookingReservedBookings  = bookingEvent.get("bookingReservedBookings") - 1;
+            bookingEvent.set("bookingReservedBookings", bookingReservedBookings);
+            var bookingAvailableBookings = bookingEvent.get("bookingAvailableBookings") + 1;
+            bookingEvent.set("bookingAvailableBookings", bookingAvailableBookings);
+            var bookingCancelledBookings = bookingEvent.get("bookingCancelledBookings") + 1;
+            bookingEvent.set("bookingCancelledBookings", bookingCancelledBookings);
+            bookingEvent.set("bookingEventStatus", 'bookingEventStatusNotActive');
+            bookingEvent.save();
+
+            bookingTicket.set("bookingTicketStatus", 'bookingTicketStatusCancelledByBusiness');
+            bookingTicket.set("bookingEventStatus", 'bookingEventStatusNotActive');
+            bookingTicket.save();
+          },
+          error: function(cancelledBooking, error) {
+            // Execute any logic that should take place if the save fails.
+            // error is a Parse.Error with an error code and message.
+            alert('Failed to create new object, with error code: ' + error.message);
+          }
+        });
+      }
+      alert(object.id + ' - ' + object.get('playerName'));
+    }
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
