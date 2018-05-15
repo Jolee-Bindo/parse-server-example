8d88203da3d1945ca4a6950a221959f81cfc8272
rouzbehezzati <35449792+rouzbehezzati@users.noreply.github.com>
2018-05-15 14:36:42 +0430

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -77,9 +77,10 @@ Parse.Cloud.define('deactivateSchedule', function(request, response) {
     var promise = Parse.Promise.as();
     _.each(bookingTickets, function(bookingTicket) {
       // For each item, extend the promise with a function to cancel it.
+       console.log(bookingTicket);
+
       promise = promise.then(function() {
         // Return a promise that will be resolved when the task is finished.
-                           console.log(bookingTicket);
 
         return cancellBookingTicket(bookingTicket);
       });
